5f1bf8b912b7c5948a26ca6941526e140d694698
aaroexxt <aaron.becker@gmail.com>
2019-09-07 15:20:36 -0700

    telnet client connects!

--- core/telnetM.js
+++ core/telnetM.js
@@ -1,37 +1,41 @@
-const telnet = require('telnet-client');
-
-//Instantiate telnet server
-const server = new telnet();
+const net = require('net');
 const EOL = "\r\n"; //end of line to server
 
 class telnetM {
 	constructor(ip = "192.168.1.33", user = "lutron", pass = "integration") {
 		console.log("Instantiating telnetM with:\nIP:"+ip+"\nUSER:"+user+"\nPASS: "+pass);
 
-		//Data handler
-		server.on("data", data => {
-			this.recv(data);
-		});
+		this.ip = ip;
+		this.login = {
+			user: user,
+			pass: pass
+		}
+		this.telnetClient = new net.Socket(); //Instantiate telnet server	
+	}
 
-		//Login
-		server.on("connect", () => {
-			server.exec(user+EOL);
-			server.exec(pass+EOL);
-			this.serverConnected();
-		});
+	begin() {
+		return new Promise((resolve, reject) => {
+			//Setup Handlers
+			this.telnetClient.on("data", data => {
+				this.recv(data);
+			});
 
-		server.connect({
-			host: ip,
-			port: 23
+			//Begin connection
+			this.telnetClient.connect(23, ip, prompt => {
+				this.serverConnected();
+				return resolve();
+			});
 		})
 	}
 
 	recv(data) {
 		console.log("Recieved data: "+data);
+		if (data.contains)
 	}
 
 	serverConnected() {
-		console.log("Server connected!");
+		console.log("Connected to telnet server@ip="+ip);
+
 	}
 }
 
